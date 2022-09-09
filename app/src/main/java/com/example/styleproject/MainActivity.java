0a20f099e194f560dc2b37d756069bccae2ccf91
nance1798 <nance1798@students.d211.org>
2022-09-09 08:10:25 -0500

    finished it all and it mostly works

--- app/src/main/java/com/example/styleproject/MainActivity.java
+++ app/src/main/java/com/example/styleproject/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.styleproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     // This is a change
@@ -11,5 +14,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onButton(View view){
+        EditText favoriteFood = findViewById(R.id.editTextFood);
+        EditText favoriteNumber = findViewById(R.id.editTextNumber);
+        TextView finalResult = findViewById(R.id.finalText);
+
+        String food = favoriteFood.getText().toString();
+        String number = favoriteNumber.getText().toString();
+
+
+        finalResult.setText("Congratulations, you have received " + number + " servings of " + food);
+
+
     }
 }
